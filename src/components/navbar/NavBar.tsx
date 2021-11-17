63aac4b140de0550fc49ae0cea49b45885f42602
Moshe Stauber <Moshe.Stauber89@gmail.com>
2021-11-16 23:48:52 -0500

    update navbar styling

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -5,7 +5,6 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <div>
-      <h5>NAVBAR</h5>
       <ul>
         <li><Link to="/Claim">Discover</Link></li>
         <li><Link to="/Create">Create</Link></li>
